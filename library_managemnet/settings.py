70772bf21deded2ed8ea6cbbd93abf91d743d4ec
Meet Patadiya <meetpatadiya980@gmail.com>
2025-10-29 19:32:50 +0530

    Added Railway deploy files

--- library_managemnet/settings.py
+++ library_managemnet/settings.py
@@ -118,13 +118,22 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
+import dj_database_url
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+DATABASES = {
+    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+}
+
+ALLOWED_HOSTS = ['*']
